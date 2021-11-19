b84272f5723eec4f1dfaf0450abbaa79474f94d6
AlperenOzcan <alperenozcanfb@gmail.com>
2021-11-19 11:00:25 +0300

    JPA repositories simplified - removed redundant repos

--- src/main/java/com/alperenozcan/insurancenotebook/dao/InsuranceQuoteRepositoryBasic.java
+++
@@ -1,9 +0,0 @@
-package com.alperenozcan.insurancenotebook.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.alperenozcan.insurancenotebook.entity.InsuranceQuote;
-
-public interface InsuranceQuoteRepositoryBasic extends JpaRepository<InsuranceQuote, Integer> {
-
-}
