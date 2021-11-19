9633461adbbc5a7c4bb7ea44372e2aefd83e80ac
AlperenOzcan <alperenozcanfb@gmail.com>
2021-11-19 09:28:34 +0300

    customer delete updated

--- src/main/java/com/alperenozcan/insurancenotebook/controller/CustomerController.java
+++ src/main/java/com/alperenozcan/insurancenotebook/controller/CustomerController.java
@@ -1,7 +1,6 @@
 package com.alperenozcan.insurancenotebook.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,26 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.alperenozcan.insurancenotebook.entity.Customer;
-import com.alperenozcan.insurancenotebook.entity.CustomerHealthDetail;
-import com.alperenozcan.insurancenotebook.entity.InsuranceQuote;
-import com.alperenozcan.insurancenotebook.service.CustomerHealthDetailService;
 import com.alperenozcan.insurancenotebook.service.CustomerService;
-import com.alperenozcan.insurancenotebook.service.InsuranceQuoteService;
 
 @Controller
 @RequestMapping("/customers")
 public class CustomerController {
 
 	private CustomerService customerService;
-	private CustomerHealthDetailService customerHealthDetailService;
-	private InsuranceQuoteService insuranceQuoteService;
 	
 	@Autowired
-	public CustomerController (CustomerService theCustomerService, CustomerHealthDetailService theCustomerHealthDetailService,
-			InsuranceQuoteService theInsuranceQuoteService) {
+	public CustomerController (CustomerService theCustomerService) {
 		customerService = theCustomerService;
-		customerHealthDetailService = theCustomerHealthDetailService;
-		insuranceQuoteService = theInsuranceQuoteService;
 	}
 	
 	@GetMapping("/list")
@@ -83,34 +73,8 @@ public class CustomerController {
 	
 	@GetMapping("/delete")
 	public String deleteCustomer(@RequestParam("customerId") int theId) {
-		
-		// Delete customer's health detail if any 
-		Optional<CustomerHealthDetail> customerHealthDetail = customerHealthDetailService.findByCustomerId(theId);
-		if (customerHealthDetail.isPresent()) {
-			int customerHealthDetailId = customerHealthDetail.get().getId();
-			customerHealthDetailService.deleteById(customerHealthDetailId);	
-		}
-				
-				
-		// Delete customer's insurance quote(s) if any 
-		Optional<List<InsuranceQuote>> insuranceQuotes = insuranceQuoteService.findByCustomerId(theId);
-		if (insuranceQuotes.isPresent()) {
-			List<InsuranceQuote> list = insuranceQuotes.get();
-			for (InsuranceQuote quote : list){
-				insuranceQuoteService.deleteById(quote.getId());
-			}
-		}
-						
-		
-		
-		Customer tempCustomer = customerService.findById(theId);
-				
-		if (tempCustomer == null) {
-			throw new RuntimeException("There is no customer with id " + theId);
-		}
-				
 		customerService.deleteById(theId);
-					
+		
 		return "redirect:/customers/list";	
 	}
 	
--- src/main/java/com/alperenozcan/insurancenotebook/service/CustomerServiceImpl.java
+++ src/main/java/com/alperenozcan/insurancenotebook/service/CustomerServiceImpl.java
@@ -5,26 +5,39 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.alperenozcan.insurancenotebook.dao.CustomerHealthDetailRepository;
 import com.alperenozcan.insurancenotebook.dao.CustomerRepository;
+import com.alperenozcan.insurancenotebook.dao.InsuranceQuoteRepository;
 import com.alperenozcan.insurancenotebook.entity.Customer;
+import com.alperenozcan.insurancenotebook.entity.CustomerHealthDetail;
+import com.alperenozcan.insurancenotebook.entity.InsuranceQuote;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
 	private CustomerRepository customerRepository;
 	
+	private CustomerHealthDetailRepository customerHealthDetailRepository;
+	private InsuranceQuoteRepository insuranceQuoteRepository;
+	
 	@Autowired
-	public CustomerServiceImpl(CustomerRepository customerRepository) {
+	public CustomerServiceImpl(CustomerRepository customerRepository, 
+			CustomerHealthDetailRepository customerHealthDetailRepository, InsuranceQuoteRepository insuranceQuoteRepository) {
 		this.customerRepository = customerRepository;
+		this.customerHealthDetailRepository = customerHealthDetailRepository;
+		this.insuranceQuoteRepository = insuranceQuoteRepository;
 	}
 
 	@Override
+	@Transactional
 	public List<Customer> findAll() {
 		return customerRepository.findAll();
 	}
 
 	@Override
+	@Transactional
 	public Customer findById(int theId) {
 		Optional<Customer> result = customerRepository.findById(theId);
 		
@@ -41,13 +54,37 @@ public class CustomerServiceImpl implements CustomerService {
 	}
 
 	@Override
+	@Transactional
 	public void save(Customer theCustomer) {
 		customerRepository.save(theCustomer);
 	}
 
+	// delete customer and other customer related records
 	@Override
+	@Transactional
 	public void deleteById(int theId) {
+		
+		Optional<CustomerHealthDetail> healthDetail = customerHealthDetailRepository.findByCustomerId(theId);
+		if(healthDetail.isPresent()) {
+			customerHealthDetailRepository.deleteById(healthDetail.get().getId());
+		}
+		
+		Optional<List<InsuranceQuote>> insuranceQuotes = insuranceQuoteRepository.findByCustomerId(theId);
+		if (insuranceQuotes.isPresent()) {
+			List<InsuranceQuote> list = insuranceQuotes.get();
+			for (InsuranceQuote quote : list){
+				insuranceQuoteRepository.deleteById(quote.getId());
+			}
+		}
+		
+		Optional<Customer> result = customerRepository.findById(theId);
+		
+		if (result.isEmpty()) {
+			throw new RuntimeException("There is no customer with id " + theId);
+		}
+		
 		customerRepository.deleteById(theId);
+		
 	}
 
 }
