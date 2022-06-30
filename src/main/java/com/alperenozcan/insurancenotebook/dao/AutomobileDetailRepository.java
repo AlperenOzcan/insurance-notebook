3c390ed11d7596c65f7fb31febdf51165f3b8476
AlperenOzcan <alperenozcanfb@gmail.com>
2022-06-30 21:40:33 +0300

    refactor repositories

--- src/main/java/com/alperenozcan/insurancenotebook/dao/AutomobileDetailRepository.java
+++ src/main/java/com/alperenozcan/insurancenotebook/dao/AutomobileDetailRepository.java
@@ -1,18 +1,12 @@
 package com.alperenozcan.insurancenotebook.dao;
 
+import com.alperenozcan.insurancenotebook.entity.AutomobileDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import com.alperenozcan.insurancenotebook.entity.AutomobileDetail;
-
 public interface AutomobileDetailRepository extends JpaRepository<AutomobileDetail, Integer>{
 
-	@Query("from AutomobileDetail where customer_id=:theCustomerId")
 	Optional<List<AutomobileDetail>> findByCustomerId(int theCustomerId);
-	
-	@Query("from AutomobileDetail where customer_id=:theCustomerId and id=:theAutomobileDetailId")
-	Optional<AutomobileDetail> findByCustomerAndAutomobileId(int theCustomerId, int theAutomobileDetailId);
 }
--- src/main/java/com/alperenozcan/insurancenotebook/dao/InsuranceQuoteRepository.java
+++ src/main/java/com/alperenozcan/insurancenotebook/dao/InsuranceQuoteRepository.java
@@ -4,23 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
 import com.alperenozcan.insurancenotebook.entity.InsuranceQuote;
 
 public interface InsuranceQuoteRepository extends JpaRepository<InsuranceQuote, Integer> {
 
-	@Query("from InsuranceQuote where detailId=:theDetailId")
 	Optional<List<InsuranceQuote>> findByDetailId(int theDetailId);
-	//Optional<List<InsuranceQuote>> findAllByDetailId(int theDetailId);
-	
-	@Query("from InsuranceQuote where detailId=:theDetailId and insuranceType=:theType")
+
 	Optional<List<InsuranceQuote>> findByDetailIdAndInsuranceType(int theDetailId, String theType);
-	// Optional<InsuranceQuote> findByDetailIdAndInsuranceType(int detailId, String insuranceType);
-	// Optional<InsuranceQuote> findAllByDetailIdAndInsuranceType(int detailId, String insuranceType);
-		
-	@Modifying
-	@Query("delete from InsuranceQuote where detailId=:theDetailId and insuranceType=:theType")
-	void deleteByDetailIdAndInsuranceType(int theDetailId, String theType);
+
+	void deleteByDetailIdAndInsuranceType(int theDetailId, String insuranceType);
 }
